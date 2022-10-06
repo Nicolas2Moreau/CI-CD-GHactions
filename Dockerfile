fb990c3e56136a4c1268e8a97638a55fe4aca6f2
Djotoon <88371439+Nicolas2Moreau@users.noreply.github.com>
2022-10-06 09:59:17 +0200

    initial setup

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM ubuntu:focal
+
+RUN mkdir /opt/apps
+COPY . /opt/apps
+
+WORKDIR /opt/apps
+
+# Install python
+RUN export DEBIAN_FRONTEND=noninteractive
+RUN apt-get update
+RUN apt-get install -y python3 python3-pip
+
+# Install python deps
+RUN pip3 install -r requirements.txt
+
+CMD [ "flask", "--app", "app.py", "run", "--host=0.0.0.0" ]
---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+
+from flask import Flask
+from pymongo import MongoClient
+
+app = Flask(__name__)
+MONGO_URI = os.getenv("MONGO_URI", "localhost")
+MONGO_PORT = os.getenv("MONGO_PORT", 27017)
+MONGO_DB_NAME = "simplon"
+
+
+def format_messages_list(message):
+    html_text = "<ul>"
+    for message in message:
+        html_text += "<li>{}</li>".format(message["text"])
+    html_text += "</ul>"
+    return html_text
+
+
+@app.route("/")
+def hello_world():
+    mongodb_client = MongoClient(MONGO_URI, MONGO_PORT)
+    simplon_db = mongodb_client[MONGO_DB_NAME]
+    return format_messages_list(simplon_db.messages.find())
